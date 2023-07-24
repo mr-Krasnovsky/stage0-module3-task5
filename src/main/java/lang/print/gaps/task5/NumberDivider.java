e9dd7c1ea700a867d302f2f63d99f692e759f60f
Maxim Krasnov <mr.krasnovsky@gmail.com>
2023-07-24 14:26:56 +0300

    Complited

--- src/main/java/lang/print/gaps/task5/NumberDivider.java
+++ src/main/java/lang/print/gaps/task5/NumberDivider.java
@@ -2,5 +2,10 @@ package lang.print.gaps.task5;
 
 public class NumberDivider {
     void divide(int toBeDivided) {
+        try {
+            System.out.println((double) toBeDivided / 5);
+        } catch (NullPointerException e) {
+            e.printStackTrace();
+        }
     }
 }
--- src/main/java/lang/print/gaps/task5/TimeConvertor.java
+++ src/main/java/lang/print/gaps/task5/TimeConvertor.java
@@ -2,5 +2,10 @@ package lang.print.gaps.task5;
 
 public class TimeConvertor {
     public void convert(float minutes) {
+        try {
+            System.out.println(minutes * 60);
+        } catch (NullPointerException e) {
+            e.printStackTrace();
+        }
     }
 }
